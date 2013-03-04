d9109c7db50b778e0f32ddaafb279f633860dc7c
fofo13 <fofosav85@hotmail.it>
2013-03-04 23:48:37 +0100

    First Commit
    version 0.0 this is a larval stage.There is only name of project with
    very few concepts,and only one package with one class.

---
+++ src/intelligent_traffic_light/StartConstruction.java
@@ -0,0 +1,16 @@
+package intelligent_traffic_light;
+
+/*The purpose of  this  class is to make up with interaction with user, a network of intelligent traffic
+ * light in a area or  zone.The  suggest is to implement a Graph structure(Map or Matrix are good 
+ * alternative for my idea)
+ * */
+public class StartConstruction {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Good luck developers");
+	}
+
+}
